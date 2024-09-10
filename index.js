faa7ebcb31f764a941c61630ca40a0f0336d9ef6
Raniel Ferreira Machado <ranielferreiramachado483@gmail.com>
2024-09-10 16:00:06 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
-//console.log("Hello World!");
+let meta = {
+  value: "Ler um livro por mês",
+  checked: true,
+};
 
-// Arrays e Objetos
-let livros = [
-  "Pai rico Pai pobre",
-  "Mais esperto que o diabo",
-  "Quem pensa enriquece",
-  "Mente Milionária",
-  "Fluent Forever",
+let metas = [
+  meta,
+  {
+    value: "Ir a academia todos os dias",
+    checked: false,
+  },
 ];
 
-//console.log(livros[0]);
-
-let checkOut = {
-  livrosParaLer: 3,
-  livrosLidos: 2,
-};
-
-//console.log(checkOut.livrosLidos);
-
-// arrow function
-
-const soma = (a, b) => {
-  return a + b;
-};
-
-//console.log(soma(2, 2));
-
-// named function
-let meusLivros = []; // Declaração inicial do array fora da função
-
-function addLivro(livro) {
-  meusLivros.push(livro); // Adiciona o livro ao array existente
-  return meusLivros;
-}
-
-addLivro("JavaScript");
-addLivro("React");
-addLivro("Node.js");
-
-console.log(meusLivros); // Imprime: ["JavaScript", "React", "Node.js"]
+console.log(metas[0].value);
