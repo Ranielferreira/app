e2f076b5323e976fde000b71edb9f41051d825f0
Raniel Ferreira Machado <ranielferreiramachado483@gmail.com>
2024-09-11 14:16:28 -0300

    fundamentos 005

--- aula.md
+++ aula.md
@@ -58,3 +58,7 @@ Arrow function / Named function
 # Estrutura de repetição
 
 While
+
+# Condicionais
+
+Switch
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const start = () => {
-  count = 0;
-  while (count <= 10) {
-    console.log(count);
-    count++;
+  while (true) {
+    let opcao = "sair";
+    switch (opcao) {
+      case "cadastrar":
+        console.log("Opção cadastrar");
+        break;
+      case "listar":
+        console.log("Opção listar");
+        break;
+      case "sair":
+        console.log("Saindo...");
+        return;
+    }
   }
 };
 
