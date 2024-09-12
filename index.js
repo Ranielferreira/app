d8d47630ae1f479dc5ca954b7747f84413c9e180
Raniel Ferreira Machado <ranielferreiramachado483@gmail.com>
2024-09-12 14:07:49 -0300

    add metas abertas

--- index.js
+++ index.js
@@ -57,8 +57,24 @@ const metasRealizadas = async () => {
   }
 
   await select({
-    message: "Metas Realizadas",
+    message: "Metas Realizadas " + realizadas.length,
     choices: [...realizadas],
+  });
+};
+
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return meta.checked != true;
+  });
+
+  if (abertas.length == 0) {
+    console.log("Não existem metas abertas.");
+    return;
+  }
+
+  await select({
+    message: "Metas abertas " + abertas.length,
+    choices: [...abertas],
   });
 };
 
@@ -80,6 +96,10 @@ const start = async () => {
           value: "realizadas",
         },
         {
+          name: "Metas abertas",
+          value: "abertas",
+        },
+        {
           name: "Sair",
           value: "sair",
         },
@@ -95,6 +115,9 @@ const start = async () => {
         break;
       case "realizadas":
         await metasRealizadas();
+        break;
+      case "abertas":
+        await metasAbertas();
         break;
       case "sair":
         console.log("Saindo...");
